e3bc8925d959edf8419630d54cbe940faf1da695
Martin Pallmann <sayhello@martinpallmann.de>
2020-05-05 12:46:31 +0200

    testing

--- build.sbt
+++ build.sbt
@@ -8,8 +8,6 @@ lazy val root = (project in file("."))
     libraryDependencies ++= Seq(
       "de.martinpallmann.gchat" %% "gchat-bot" % "0.0.17",
       "ch.qos.logback" % "logback-classic" % "1.2.3",
-      "com.google.api-client" % "google-api-client" % "1.30.9",
-      "com.google.http-client" % "google-http-client-jackson" % "1.29.2",
       "com.auth0" % "java-jwt" % "3.10.2",
       "io.circe" %% "circe-parser" % "0.13.0",
       "io.monix" %% "minitest" % "2.8.2" % Test,
--- src/main/scala/familybot/Jwt.scala
+++ src/main/scala/familybot/Jwt.scala
@@ -1,74 +1,30 @@
 package familybot
 
-import java.net.URL
-import java.nio.charset.StandardCharsets
-import java.security.KeyFactory
-import java.security.interfaces.{RSAPrivateKey, RSAPublicKey}
-import java.security.spec.X509EncodedKeySpec
-import java.util.Base64
-
 import com.auth0.jwt.{JWT, JWTVerifier}
 import com.auth0.jwt.algorithms.Algorithm
-import com.auth0.jwt.interfaces.RSAKeyProvider
-import com.google.api.client.googleapis.auth.oauth2.GooglePublicKeysManager
-import com.google.api.client.http.apache.ApacheHttpTransport
-import com.google.api.client.json.jackson.JacksonFactory
 import familybot.auth.KeyProvider
-import io.circe.{Json, JsonObject}
 import org.slf4j.LoggerFactory
-
-import scala.jdk.CollectionConverters._
 import scala.util.{Failure, Success, Try}
 import scala.concurrent.duration._
-import scala.io.Source
-import io.circe.parser.parse
 
 object Jwt {
 
   private val logger = LoggerFactory.getLogger(getClass)
 
-  val CHAT_ISSUER = "chat@system.gserviceaccount.com"
-  val PUBLIC_CERT_URL_PREFIX =
-    "https://www.googleapis.com/service_accounts/v1/metadata/x509/"
-
-  private val keyManager = {
-    val factory = new JacksonFactory()
-    val keyManagerBuilder =
-      new GooglePublicKeysManager.Builder(new ApacheHttpTransport, factory)
-    val certUrl = PUBLIC_CERT_URL_PREFIX + CHAT_ISSUER
-    keyManagerBuilder.setPublicCertsEncodedUrl(certUrl)
-    keyManagerBuilder.build()
-  }
-
-  def verifier: JWTVerifier = {
-    JWT.require(Algorithm.RSA256(KeyProvider())).build()
-  }
-
-  def publicKeys: List[RSAPublicKey] =
-    keyManager.getPublicKeys.asScala.toList
-      .filter(_.isInstanceOf[RSAPublicKey])
-      .map(_.asInstanceOf[RSAPublicKey])
-
-  def verifiers: List[String => Boolean] =
-    publicKeys.map(k => {
-      val v = JWT
-        .require(Algorithm.RSA256(k, null))
-        .acceptLeeway(5.minutes.toSeconds)
-        .withIssuer("chat@system.gserviceaccount.com")
-        .withAudience("301972490637") // Google project number
-        .build()
-      s: String =>
-        Try {
-          v.verify(s)
-        }.isSuccess
-    })
+  def verifier: JWTVerifier =
+    JWT
+      .require(Algorithm.RSA256(KeyProvider()))
+      .acceptLeeway(5.minutes.toSeconds)
+      .withIssuer("chat@system.gserviceaccount.com")
+      .withAudience("301972490637")
+      .build()
 
   def verify(token: String): Unit = {
     Try {
       verifier.verify(token)
     } match {
-      case Success(_) => logger.debug("token is valid")
-      case Failure(e) => logger.debug("token is invalid")
+      case Success(_) => logger.debug("Token is valid")
+      case Failure(e) => logger.debug("Token is invalid")
     }
   }
 }
